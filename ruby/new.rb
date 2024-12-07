178779e33eeb207b8c074eb4ab9dfdc677bdb097
yoshihiro201104 <187289075+yoshihiro201104@users.noreply.github.com>
2024-12-07 11:50:47 +0000

    [Add]4-9

---
+++ ruby/new.rb
@@ -0,0 +1,8 @@
+# クラスメソッド
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
---
+++ ruby/symbol.rb
@@ -0,0 +1,16 @@
+webcamp = :プログラミング学習
+puts webcamp
+
+#文字列オブジェクト
+dog1 = "犬"
+dog2 = "犬"
+
+
+#シンボルオブジェクト
+dog3 = :犬
+dog4 = :犬
+
+puts dog1.object_id
+puts dog2.object_id
+puts dog3.object_id
+puts dog4.object_id
